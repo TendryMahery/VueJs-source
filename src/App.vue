2579b2912cc5020733eb51212a4d26547edb5d18
TendryMahery <rafanomezanatendrymahery@gmail.com>
2021-12-08 21:41:14 +0300

    vueJs3-source

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>app</h1>
+</template>
+
+<script>
+
+import Teste from './components/Teste.vue'
+
+export default {
+  name: 'App',
+  cpmponents: {Teste}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Teste.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <h1>coucou</h1>
+</template>
+
+<script>
+export default {
+  name: 'Teste'
+ 
+}
+</script>
+ 
+<style scoped>
+
+</style>
